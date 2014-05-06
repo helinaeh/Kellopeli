97d8d7cd596ae16d20108b574c387310e04db74c
touney <touney@gmail.com>
2014-05-06 18:41:37 +0300

    algoritmi ei ollukaan valmis...

--- js/algoritmiluonnos.js
+++ js/algoritmiluonnos.js
@@ -4,7 +4,7 @@ saa vielä hirveesti ideaa selville kun on vielö niin kesken...
 */
 
 var counter = 0; // +1 aina kun uusi seinämä tehdään
-var obsProb = 0.1; // todnäk jolla seinään tulee "avattava" este
+var obsProp = 0.1; // todnäk jolla seinään tulee "avattava" este
 var yArray = [];
 var obsArray = [];
 
@@ -29,8 +29,8 @@ function Algoritmi(car, hom, gar, wor, watch) { // Parametrit ovat true/false-ar
 
 function addHole() {
 	var position = randomPosition(1);
-	yArray += position;
-	yArray += position + 1;
+	if () yArray += position;
+	if (count < 50 || Math.random() <= obsProp) yArray += position + 1;
 };
 
 function addObstacles(car, hom, gar, wor, watch) {
@@ -38,38 +38,52 @@ function addObstacles(car, hom, gar, wor, watch) {
 	position = 0;
 
 	for (i = 0; i < count % 40; i++) { //lisätään työpaikka (ehkä)
-		if(Math.random() <= obsProb * 0.4) {
+		if(Math.random() <= obsProp * 0.4) {
 			position = randomPosition(4);
-			obsArray += new Home(position, wor || watch);
-			yArray += position; //äsken lisätyn esteen "indeksit" (kts. ylempää)
-			a = a + 1; // turha??
+			if (!(position == -1)) {
+				obsArray += new Home(position, wor || watch);
+				yArray += position; //äsken lisätyn esteen "indeksit" (kts. ylempää)
+				yArray += position + 1;
+				yArray += position + 2;
+				yArray += position + 3;
+				a = a + 1; // turha??
+			};
 		};
 	}
 
 	for (i = 0; i < count % 30; i++) { //lisätään koti (ehkä)
-		if(Math.random() <= obsProb * 0.6) {
+		if(Math.random() <= obsProp * 0.6) {
 			position = randomPosition(3);
-			obsArray += new Home(position, hom || watch);
-			yArray += position; //äsken lisätyn esteen "indeksit" (kts. ylempää)
-			a = a + 1; // turha??
+			if (!(position == -1)) {
+				obsArray += new Home(position, hom || watch);
+				yArray += position; //äsken lisätyn esteen "indeksit" (kts. ylempää)
+				yArray += position + 1;
+				yArray += position + 2;
+				a = a + 1; // turha??
+			};
 		};
 	}
 
 	for (i = 0; i < count % 20; i++) { //lisätään autotalli (ehkä)
-		if(Math.random() <= obsProb * 0.8) {
+		if(Math.random() <= obsProp * 0.8) {
 			position = randomPosition(2);
-			obsArray += new Garage(position, gar || watch);
-			yArray += position; //äsken lisätyn esteen "indeksit" (kts. ylempää)
-			a = a + 1; // turha??
+			if (!(position == -1)) {
+				obsArray += new Garage(position, gar || watch);
+				yArray += position; //äsken lisätyn esteen "indeksit" (kts. ylempää)
+				yArray += position + 1;
+				a = a + 1; // turha??
+			};
 		};
 	}
 
 	for (i = 0; i < count % 10; i++) { //lisätään auto (ehkä)
-		if(Math.random() <= obsProb) {
-			var position = randomPosition(1);
-			obsArray += new Car(position, car || watch);
-			yArray += position; //äsken lisätyn esteen "indeksit" (kts. ylempää)
-			a = a + 1; // turha??
+		if(Math.random() <= obsProp) {
+			position = randomPosition(1);
+			if (!(position == -1)) {
+				obsArray += new Car(position, car || watch);
+				yArray += position; //äsken lisätyn esteen "indeksit" (kts. ylempää)
+				a = a + 1; // turha??
+			};
 		};
 	}
 };
@@ -83,10 +97,20 @@ function addWalls() {
 };
 
 function randomPosition(height) {
-	var result = 0;
+	var result = -1;
+	/*
+	ensimmäinen versio, tämä olisi jatkanut paikan arpomista loputtomiin jos tarpeeksi isoa paikkaa ei ole
 	do {
 		result = Math.round(Math.random() * 10);
 	} while (!isEmpty(result, height));
+	*/
+	for (i = 0; i < 10; i++) {
+		result = Math.round(Math.random() * 10);
+		if (isEmpty(result, height)) {
+			return result;
+		} else result = -1;
+	}
+
 	result;
 };
 
